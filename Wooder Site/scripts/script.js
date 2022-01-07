0eedeff11d813c31e741cff6a33a196fd1957154
Rolan-Tol <80161227+Rolan-Tol@users.noreply.github.com>
2022-01-07 21:17:51 +0200

    ver 1.hz kakaya
    Добавлено все, кроме декоративных и еле заметных надписей(в силу улучшения разработки масштабирования сайта)

--- Wooder Site/scripts/script.js
+++ Wooder Site/scripts/script.js
@@ -1,6 +1,6 @@
 learnButton = document.getElementById('learnbutton');
 learnArrow = document.getElementById('learnarrow');
-learnText = document.getElementById('learntext')
+learnText = document.getElementById('learntext');
 
 learnButton.onmouseover = function() {
     learnArrow.classList.add('hover')
@@ -13,4 +13,59 @@ learnButton.onmouseout = function() {
     learnText.classList.remove('hover')
     learnButton.classList.remove('hover')
 }
-    
+
+let learnMore = document.querySelectorAll('.learnmorejs')
+for(let i=0; i< learnMore.length; i++) {
+        var e = document.getElementById('learnmore');
+        e.id = "learnmore"+i;;
+    }
+
+let learnMoreArrow = document.querySelectorAll('.learnmorearrowjs')
+for(let i=0; i< learnMoreArrow.length; i++) {
+        var f = document.getElementById('learnmorearrow');
+        f.id = "learnmorearrow"+i;
+    }
+
+// Первая карточка
+document.getElementById('learnmore0').onmouseover = function() {
+    document.getElementById('learnmorearrow0').classList.add('hover')
+    document.getElementById('learnmore0').classList.add('hover')
+}
+
+document.getElementById('learnmore0').onmouseout = function() {
+    document.getElementById('learnmorearrow0').classList.remove('hover')
+    document.getElementById('learnmore0').classList.remove('hover')
+}
+
+// Вторая карточка 
+document.getElementById('learnmore1').onmouseover = function() {
+    document.getElementById('learnmorearrow1').classList.add('hover')
+    document.getElementById('learnmore1').classList.add('hover')
+}
+
+document.getElementById('learnmore1').onmouseout = function() {
+    document.getElementById('learnmorearrow1').classList.remove('hover')
+    document.getElementById('learnmore1').classList.remove('hover')
+}
+
+// Третья карточка
+document.getElementById('learnmore2').onmouseover = function() {
+    document.getElementById('learnmorearrow2').classList.add('hover')
+    document.getElementById('learnmore2').classList.add('hover')
+}
+
+document.getElementById('learnmore2').onmouseout = function() {
+    document.getElementById('learnmorearrow2').classList.remove('hover')
+    document.getElementById('learnmore2').classList.remove('hover')
+}
+
+watchVideoButton = document.querySelector('.description-heading .button-frame')
+watchVideoText = document.querySelector('.description-heading .button-frame > .button')
+
+watchVideoButton.onmouseover = function() {
+    watchVideoText.classList.add('hover')
+}
+
+watchVideoButton.onmouseout = function() {
+    watchVideoText.classList.remove('hover')
+}
